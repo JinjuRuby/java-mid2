b2d660235a9978e1c0168218e5d8f0fbbbbeeba5
JinjuRuby <ronnie0304@naver.com>
2025-03-27 19:21:24 +0900

    java

---
+++ src/collection/map/test/WordFrequencyTest2.java
@@ -0,0 +1,26 @@
+package collection.map.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class WordFrequencyTest2 {
+
+    public static void main(String[] args) {
+
+        String text = "orange banana apple apple banana apple";
+        String[] words = text.split(" ");
+
+        Map<String, Integer> map = new HashMap<>();
+
+        for (String word : words) {
+            Integer count = map.getOrDefault(word, 0);
+            count++;
+            map.put(word, count);
+        }
+
+
+
+        System.out.println(map);
+
+    }
+}
